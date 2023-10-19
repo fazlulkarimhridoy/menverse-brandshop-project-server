c02d15a93507efe8fe660cb2d0c59121b6423a2f
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2023-10-19 21:09:34 +0600

    added

--- index.js
+++ index.js
@@ -1,48 +1,38 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config(); 
 const port = process.env.PORT || 5000;
+const app = express();
 
-// middlewares
+// used Middleware
 app.use(cors());
 app.use(express.json());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ibuouo3.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Connact With MongoDb Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.krxly.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+// Create a async fucntion to all others activity
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // Create Database to store Data
+    const testCollection = client.db("testDatabaseName").collection("testData");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-// call the run function
+// Call the function you declare abobe
 run().catch(console.dir);
-
+// Root Api to check activity
 app.get("/", (req, res) => {
-  res.send("SIMPLE CRUD IS RUNNING");
+  res.send("Hello From NR Computers!");
 });
-
 app.listen(port, () => {
-  console.log(`SIMPLE CURD is running on port, ${port}`);
+  console.log(`NR Computers listening on port ${port}`);
 });
